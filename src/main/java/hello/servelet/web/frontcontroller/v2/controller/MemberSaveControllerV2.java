20316f89d0587053466e75e80dc9cc1b73386d03
양희준 <>
2021-04-30 16:18:18 +0900

    frontController V2

---
+++ src/main/java/hello/servelet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -0,0 +1,30 @@
+package hello.servelet.web.frontcontroller.v2.controller;
+
+import hello.servelet.domain.member.Member;
+import hello.servelet.domain.member.MemberRepository;
+import hello.servelet.web.frontcontroller.MyView;
+import hello.servelet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        return new MyView("/WEB-INF/views/save-result.jsp");
+
+    }
+}
