61658be26e0b43d117e4b2c55df436b922144e53
양희준 <>
2021-04-30 17:31:04 +0900

    frontControllerV3 Servlet 종속성 제거

---
+++ src/main/java/hello/servelet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -0,0 +1,24 @@
+package hello.servelet.web.frontcontroller.v3.controller;
+
+import hello.servelet.domain.member.Member;
+import hello.servelet.domain.member.MemberRepository;
+import hello.servelet.web.frontcontroller.ModelView;
+import hello.servelet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV3 implements ControllerV3 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        List<Member> members = memberRepository.findAll();
+        ModelView mv = new ModelView("members");
+        mv.getModel().put("members", members);
+
+        return mv;
+
+    }
+}
